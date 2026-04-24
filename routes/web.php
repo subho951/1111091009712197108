2554820c60beadcbd0f97dc1da1d55941402fab8
subhomoy samanta <subhomoy@keylines.net>
2026-04-24 09:07:34 +0530

    delete account request

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::match(['get', 'post'], '/forgot-password', 'App\Http\Controllers\FrontCon
 Route::match(['get', 'post'], '/validate-otp/{id}', 'App\Http\Controllers\FrontController@validateOTP');
 Route::match(['get', 'post'], '/reset-password/{id}', 'App\Http\Controllers\FrontController@resetPassword');
 Route::match(['get', 'post'], '/logout', 'App\Http\Controllers\FrontController@signout');
+Route::post('delete-account-update', [FrontController::class, 'deleteaccount'])->name('delete-account.store');
 
 Route::match(['get', 'post'], '/home', 'App\Http\Controllers\FrontController@home');
 
